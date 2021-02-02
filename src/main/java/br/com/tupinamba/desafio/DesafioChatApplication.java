df9c2679553e6701e5c25b92483bec20e9b5509c
gbuenor <gbuenor@live.com>
2021-02-02 11:46:20 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/tupinamba/desafio/DesafioChatApplication.java
@@ -0,0 +1,13 @@
+package br.com.tupinamba.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioChatApplication.class, args);
+	}
+
+}
