526e35643a9dfc42e2bda17af45871997fb6150b
gbuenor <gbuenor@live.com>
2021-02-08 02:00:10 -0300

    Tratamento de erros

--- src/main/java/br/com/tupinamba/desafio/repository/impl/MessageRepositoryImpl.java
+++ src/main/java/br/com/tupinamba/desafio/repository/impl/MessageRepositoryImpl.java
@@ -2,24 +2,39 @@ package br.com.tupinamba.desafio.repository.impl;
 
 import br.com.tupinamba.desafio.entyties.MessageEntity;
 import br.com.tupinamba.desafio.repository.MessageRepository;
+import br.com.tupinamba.desafio.utils.ProjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class MessageRepositoryImpl {
 
     @Autowired
     private MessageRepository messageRepository;
 
+    Logger logger = Logger.getLogger("br.com.tupinamba.desafio.repository.impl.MessageRepositoryImpl");
+
+    @Autowired
+    private ProjectUtils utils;
+
     public List<MessageEntity> getMessages() {
-        return this.messageRepository.findAll();
+        try {
+            return this.messageRepository.findAll();
+        } catch (Exception e) {
+            throw this.utils.createException("Não foi possivel obter as mensagens da base de dados");
+        }
     }
 
     public MessageEntity insertNewMessage(MessageEntity messageEntity) {
         messageEntity.setDateMessage(LocalDateTime.now(ZoneId.systemDefault()));
-        return this.messageRepository.save(messageEntity);
+        try {
+            return this.messageRepository.save(messageEntity);
+        } catch (Exception e) {
+            this.logger.info("Não foi possivel inserir mensagem na base de dados");
+        }
+        return messageEntity;
     }
-
 }
